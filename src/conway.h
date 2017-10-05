0c33fca2f409e89868c73bda1de820d4955ab0ce
smaillot <stephane.bernard.maillot@estudiant.upc.edu>
2017-10-05 23:39:00 +0200

    Compiled

--- src/conway.h
+++ src/conway.h
@@ -1,5 +1,5 @@
 struct Cell {int x; int y; bool alive;};
-struct Map {bool** world[10][10]; bool** next_world[10][10]; int rows; int cols;};
+struct Map {bool world[10][10]; bool next_world[10][10]; int rows; int cols;};
 
 int initall(void);
 int enditall(void);
