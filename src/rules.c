0aa04bcfbcdbea3313d137ddc1af0aba5e65f5b2
Lagerbe <30956718+Lagerbe@users.noreply.github.com>
2017-10-27 00:53:53 +0200

    Commentary on rules

--- src/rules.c
+++ src/rules.c
@@ -6,6 +6,10 @@
 int count_neighbors (struct Cell *c, struct Map *m) {
 	// coder 2
     // Count the number of neighbors of a given cell
+    
+    // This function take the adresses of a cell and its current map.
+    // It extract the coordinates x and y of the cell and check line by line
+    // if one of his neighboor is live or dead.
 
     int num_live=0;
     int x=c->x;
@@ -16,9 +20,11 @@ int count_neighbors (struct Cell *c, struct Map *m) {
         for (int j=-1; j<2; j++)
         {
             if (j==0 && k==0){
-            }
+            } // This condition is here to avoid to take the life of the cell studied into account
             else {
                 if (m->world[x+k][y+j])
+                    // the map has 2 possible values in one position True or False,
+                    // here it is equivalent to write if (True)
                 {
                     num_live++;
                 }
@@ -32,6 +38,7 @@ bool compute_next_state (bool alive, int num_live) {
 	//coder 2
     // Given the current state and the # of neighbors of a cell, apply the rules to compute the next step
 
+    // The rules only need to now if a cell is alive or not and its number of neighbor
 	bool state;
     if (alive && num_live<=3 && num_live>=2)
     {
@@ -52,7 +59,7 @@ bool get_next_state (struct Cell *c, struct Map *m)
 {
 	// coder 2
     // Return the next state given the cell structure and the current world map
-
+    
 	bool alive = c->alive;
 	int num_live = count_neighbors(c, m);
 
