397e75733d89fb0900dde0118cb79c412e795205
smaillot <stephane.bernard.maillot@estudiant.upc.edu>
2017-10-27 02:44:52 +0200

    Final modifications

--- src/conway.h
+++ src/conway.h
@@ -1,3 +1,5 @@
+// Game constants
+
 #define LIVE_SYMBOL		(char)0x2588 // solid block ascii code
 #define BORDER_SYMBOL	'+'//(char)0x2588 // solid block ascii code
 #define COLS			150
@@ -7,29 +9,44 @@
 #define LEFT_EDGE   	0  
 #define RIGHT_EDGE  	COLS +1	
 
+
+// Structures
+
 struct Cell {int x; int y; bool alive;};
 struct Point {int x; int y;};
 struct Map {bool world[ROWS+2][COLS+2]; bool next_world[ROWS+2][COLS+2]; int rows; int cols;};
+
+// Display functions
 
 void initall(void);
 void enditall(void);
 
 void display(struct Cell*);
-int count_neighbors(struct Cell*, struct Map*);
-
-bool get_next_state (struct Cell*, struct Map*);
-
-void display_interface(struct Map*);
+int display_interface(struct Map*);
 
 void inimap();
 void pixel(int, int, char);
 void display_cell(struct Cell*); 
 void display_map(struct Map*);
 
+
+// Game rules
+
+int count_neighbors(struct Cell*, struct Map*);
+bool get_next_state (struct Cell*, struct Map*);
+
+
+// Drawing
+
 void animate(int);
 void draw_cell(int, int, struct Map*);
 void draw_shape(int, int, int, struct Point*, struct Map*);
 void draw_shape_180(int, int, int, struct Point*, struct Map*);
+void draw_shape_flip_h(int, int, int, struct Point*, struct Map*);
+void draw_shape_flip_v(int, int, int, struct Point*, struct Map*);
+
+
+// Patterns
 
 extern struct Point onepoint[1];
 extern struct Point twopoints[2]; 
@@ -40,3 +57,8 @@ extern struct Point oscillator[28];
 extern struct Point cross[28];
 extern struct Point galaxy[48];
 extern struct Point canon[36];
+extern struct Point pulsar[14];
+
+extern struct Point pentomino[5];
+extern struct Point diehard[7];
+extern struct Point acorn[7];
